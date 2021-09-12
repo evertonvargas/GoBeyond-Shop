a9390f0f0fde9faa127e8daddeaf483adcc1980e
evertonvargas <evertonpassa7@gmail.com>
2021-09-12 19:43:56 -0300

    add menu dropdown

--- src/components/Header/Menu/index.tsx
+++ src/components/Header/Menu/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { useMenu } from "../../../hooks/useMenu";
 
@@ -5,29 +6,49 @@ import styles from "./styles.module.scss";
 
 export function Menu() {
   const { menu } = useMenu();
+  const [dropdownCategory1, setDropdownCategory1] = useState(false);
+  const [dropdownCategory2, setDropdownCategory2] = useState(false);
 
   return (
-    <div className={menu ? styles.MenuHamburguer : styles.Container }>
+    <div className={menu ? styles.MenuHamburguer : styles.Container}>
       <nav className={menu ? styles.MenuHamburguer : null}>
         <ul>
           <li>
             <a href="">Home</a>
           </li>
-          <li>
-            <a href="">
-              Categoria 1 <IoMdArrowDropdown />
-            </a>
+          <li
+            className={styles.drop}
+            onMouseOver={() => setDropdownCategory1(true)}
+            onClick={() => setDropdownCategory1(!dropdownCategory1)}
+          >
+            Categoria 1 <IoMdArrowDropdown />
+            {dropdownCategory1 && (
+              <div onMouseLeave={() => setDropdownCategory1(false)}>
+                <a href="">Sapatos Pretos</a>
+                <a href="">Blusas</a>
+                <a href="">Roupas</a>
+              </div>
+            )}
           </li>
-          <li>
-            <a href="">
-              Categoria 2 <IoMdArrowDropdown />
-            </a>
+          <li
+            className={styles.drop}
+            onMouseOver={() => setDropdownCategory2(true)}
+            onClick={() => setDropdownCategory2(!dropdownCategory2)}
+          >
+            Categoria 2 <IoMdArrowDropdown />
+            {dropdownCategory2 && (
+              <div onMouseLeave={() => setDropdownCategory2(false)}>
+                <a href="">Esporte</a>
+                <a href="">Casacos</a>
+                <a href="">Vestidos</a>
+              </div>
+            )}
           </li>
           <li>
             <a href="">Contato </a>
           </li>
         </ul>
-      </nav>       
+      </nav>
     </div>
   );
 }
