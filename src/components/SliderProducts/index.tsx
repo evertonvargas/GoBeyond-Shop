d2820dc2f261c9ba5a451c59463101a6c33f5f48
evertonvargas <evertonpassa7@gmail.com>
2021-09-12 20:17:21 -0300

    refatorando o header e o slider

--- src/components/SliderProducts/index.tsx
+++ src/components/SliderProducts/index.tsx
@@ -1,43 +1,23 @@
 import { Card } from "../Card";
-import { useState } from "react";
 
 import styles from "./styles.module.scss";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 import { products } from "../../../products";
 
-function getWindowDimensions() {
-  const { innerWidth: width } = window;
-  return {
-    width,
-  };
-}
-
 export function SliderProducts() {
-  const [slide, setSlide] = useState(0);
-  const [number, setNumber] = useState(0);
-
-  function handlePrevSlide() {
-    console.log(slide)
-    setSlide(slide - 1 < 0 ? 5 - 1 : slide - 1);
-  }
-
-  function handleNextSlide() {
-    console.log(slide)
-    setSlide(slide + 1 >= 5 ? 0 : slide + 1);
-  }
-
+  
   return (
     <section className={styles.slideProducts}>
       <h2>Mais Vendidos</h2>
       <div className={styles.slide}>
-        <button className={styles.prevSlide} onClick={handlePrevSlide}>
+        <button className={styles.prevSlide} >
           <MdKeyboardArrowLeft size={50} />
         </button>
         {products.map((product) => (
           <Card key={product.id} img={product.image} title={product.title} priceOld={product.priceOld} price={product.price} />
         ))}
-        <button className={styles.nextSlide} onClick={handleNextSlide}>
+        <button className={styles.nextSlide}>
           <MdKeyboardArrowRight size={50} />
         </button>
       </div>
